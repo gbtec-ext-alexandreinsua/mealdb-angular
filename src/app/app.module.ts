fcce45996ca0a3dc6b476412cd990ad0bf879192
Alexandre Insua <92730122+gbtec-ext-alexandreinsua@users.noreply.github.com>
2025-02-05 08:31:37 +0100

    feat: enhance styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
-import { FormsModule } from '@angular/forms';
 import { AboutComponent } from './about/about.component';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { MealCardComponent } from './meals/meal-card/meal-card.component';
 import { MealDetailComponent } from './meals/meal-detail/meal-detail.component';
@@ -19,12 +19,12 @@ import { MealListComponent } from './meals/meal-list/meal-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
     AboutComponent,
+    AppComponent,
     ContactComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
     MealListComponent,
     MealDetailComponent,
     MealCardComponent,
